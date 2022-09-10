17089877fa6d5c3a4091e4115c27ab6ea66c9a3f
wonth93 <wongth93@gmail.com>
2022-09-10 08:58:12 +0000

    add testing code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,16 @@
 const request = require('request');
-const theBreed = process.argv[2];
 
-const breedFetcher = function(breed, message) {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-    if (error !== null) {
-      return console.log(error);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
     }
-    return message(body);
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback('Breed not found!', null);
+    }
+    return callback(null, data[0]['description']);
   });
 };
 
-const pullOutMsg = function(cat) {
-  const data = JSON.parse(cat);
-  if (data.length === 0) {
-    return console.log('Breed not found!');
-  }
-  return console.log(data[0]['description']);
-};
-
-breedFetcher(theBreed, pullOutMsg);
+module.exports = { fetchBreedDescription };
